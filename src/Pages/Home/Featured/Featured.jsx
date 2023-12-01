7cd54e1b01e8f2c06c277d945107069d1b5f4aa8
tarekul42 <tarekulrifat205@gmail.com>
2023-12-01 09:41:06 -0800

    Create Parallax and Add Testimonials with react rating

--- src/Pages/Home/Featured/Featured.jsx
+++ src/Pages/Home/Featured/Featured.jsx
@@ -6,7 +6,7 @@ import './featured.css'
 
 const Featured = () => {
     return (
-        <div className="featured-item text-white pt-8 my-20">
+        <div className="featured-item bg-fixed text-white pt-8 my-20">
             <SectionTitle
                 subHeading={"Check it out"}
                 heading={"Featured item"}
@@ -19,7 +19,7 @@ const Featured = () => {
                     <p>Aug 20, 2029</p>
                     <p className="uppercase">Where can i get some?</p>
                     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo eveniet est beatae labore quasi illum repudiandae magni, voluptatibus in nemo error incidunt vel adipisci accusamus mollitia iusto nesciunt temporibus ipsam unde quae esse. Deleniti corporis similique, ut accusamus et quidem nulla quam accusantium consectetur libero. Voluptas minus placeat sint sapiente?</p>
-                    <button className="btn btn-outline">Order Now</button>
+                    <button className="btn btn-outline border-0 border-b-4 mt-4">Order Now</button>
                 </div>
             </div>
         </div>
---
+++ src/Pages/Home/Testimonials/Testimonials.jsx
@@ -0,0 +1,60 @@
+import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+// import required modules
+import { Pagination, Navigation } from 'swiper/modules';
+// import ratings
+import { Rating } from "@smastrom/react-rating";
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import { useEffect, useState } from "react";
+// import ratings style
+import '@smastrom/react-rating/style.css'
+
+const Testimonials = () => {
+    const [reviews, setReviews] = useState([]);
+
+    useEffect(() => {
+        fetch("reviews.json")
+            .then(res => res.json())
+            .then(data => setReviews(data))
+    }, [])
+    return (
+        <section className="my-20">
+            <SectionTitle
+                subHeading={"What Our Client Say"}
+                heading={"Testimonials"}
+            />
+            <Swiper
+                pagination={{
+                    type: 'progressbar',
+                }}
+                navigation={true}
+                modules={[Pagination, Navigation]}
+                className="mySwiper"
+            >
+
+                {
+                    reviews.map(review => <SwiperSlide
+                        key={review._id}
+                    >
+                        <div className="flex flex-col items-center mx-24 my-16">
+                            <Rating
+                                style={{ maxWidth: 180 }}
+                                value={review.rating}
+                                readOnly
+                            />
+                            <p className="py-8">{review.details}</p>
+                            <h3 className="text-2xl text-amber-400">{review.name}</h3>
+                        </div>
+                    </SwiperSlide>)
+                }
+            </Swiper>
+        </section>
+    );
+};
+
+export default Testimonials;
