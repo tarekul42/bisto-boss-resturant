c61ed7895de64b5b1cdccf68c10bc955292e458b
tarekul42 <tarekulrifat205@gmail.com>
2023-12-01 21:51:17 -0800

    Select right tab based on the category

--- src/Pages/Order/Order/Order.jsx
+++ src/Pages/Order/Order/Order.jsx
@@ -7,11 +7,14 @@ import 'react-tabs/style/react-tabs.css';
 import useMenu from '../../../Hooks/useMenu';
 import OrderTab from '../OrderTab/OrderTab';
 import { useParams } from 'react-router-dom';
+import { Helmet } from 'react-helmet-async';
 
 const Order = () => {
-    const [tabIndex, setTabIndex] = useState(0);
-    const [menu] = useMenu();
+    const categories = [ 'salad', 'pizza', 'soup', 'dessert', 'drinks'];
     const { category } = useParams();
+    const initialIndex = categories.indexOf(category);
+    const [tabIndex, setTabIndex] = useState(initialIndex);
+    const [menu] = useMenu();
     const desserts = menu.filter(item => item.category === 'dessert');
     const pizza = menu.filter(item => item.category === 'pizza');
     const salad = menu.filter(item => item.category === 'salad');
@@ -21,6 +24,11 @@ const Order = () => {
 
     return (
         <div>
+            {/* use helmet here */}
+            <Helmet>
+                <title>Bistro Boss | Order Food</title>
+            </Helmet>
+            {/* main page started from here */}
             <Cover img={orderCover} title={"Order Food"} />
             <Tabs defaultIndex={tabIndex} onSelect={(index) => setTabIndex(index)}>
                 <TabList>
