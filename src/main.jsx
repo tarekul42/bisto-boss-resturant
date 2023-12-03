943b16e9bc7bd6692467b7b0ab635c2475a7cd4f
tarekul42 <tarekulrifat205@gmail.com>
2023-12-02 18:00:02 -0800

    Simple Overview of React Query and load data

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import { RouterProvider } from 'react-router-dom'
-import { router } from './Routes/Routes.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './Routes/Routes.jsx';
 import { HelmetProvider } from 'react-helmet-async';
-import AuthProvider from './Providers/AuthProvider.jsx'
+import AuthProvider from './Providers/AuthProvider.jsx';
+import { QueryClient, QueryClientProvider, } from '@tanstack/react-query';
+
+// Create a client
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthProvider>
       <HelmetProvider>
-        <div className='max-w-screen-xl mx-auto'>
-          <RouterProvider router={router} />
-        </div>
+        <QueryClientProvider client={queryClient}>
+          <div className='max-w-screen-xl mx-auto'>
+            <RouterProvider router={router} />
+          </div>
+        </QueryClientProvider>
       </HelmetProvider>
     </AuthProvider>
   </React.StrictMode>,
