622f6c629843382ba3edbf7af53d3fd849699bbc
tarekul42 <tarekulrifat205@gmail.com>
2023-12-04 17:17:58 -0800

    Display cart items to review before order

--- src/Layout/DashBoard.jsx
+++ src/Layout/DashBoard.jsx
@@ -5,15 +5,15 @@ import useCart from "../Hooks/useCart";
 const Dashboard = () => {
     const [cart] = useCart();
     return (
-        <div className="drawer drawer-mobile ">
+        <div className="drawer lg:drawer-open">
             <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
             <div className="drawer-content flex flex-col items-center justify-center">
                 <Outlet></Outlet>
                 <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">Open drawer</label>
 
             </div>
-            <div className="drawer-side">
-                <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
+            <div className="drawer-side bg-[#D1A054]">
+                <label htmlFor="my-drawer-2" aria-label="close sidebar" className="drawer-overlay"></label>
                 <ul className="menu p-4 w-80">
 
                     <li><NavLink to="/dashboard/home"><FaHome></FaHome> User Home</NavLink></li>
